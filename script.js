6eb8a08de63fa781488e87b3c26ea813e7d5e8b6
Tomas Breuckmann <tomas.yoga@yahoo.com>
2021-12-14 17:29:10 -0300

    project todo list - criterio 7 - npm test ok

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+
 function adicionaTarefa(){
     let valor=document.getElementById("texto-tarefa").value;
-    console.log(valor);
     let lista=document.getElementById('lista-tarefas')
     let item=document.createElement('li')
     item.innerText=valor;
+    item.style.backgroundColor='none'
     lista.appendChild(item)
     document.getElementById('texto-tarefa').value=''
 }
+
+function pintaFundo(event){
+    let lista=document.getElementById('lista-tarefas')
+    lista.addEventListener('click',function(event){
+        event.target.style.backgroundColor='rgb(128, 128, 128)'
+    })
+}
+pintaFundo()
