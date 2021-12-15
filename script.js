d925792e98edf28aca6c7b69bb696d46ac28cc40
Tomas Breuckmann <tomas.yoga@yahoo.com>
2021-12-15 14:52:17 -0300

    project todo list - criterio 9 - npm test ok

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-
+// adiciona a tarefa ao clicar no botão
 function adicionaTarefa(){
     let valor=document.getElementById("texto-tarefa").value;
     let lista=document.getElementById('lista-tarefas')
     let item=document.createElement('li')
     item.innerText=valor;
-    item.classList.add('cor')
+    if (valor==''){
+        alert('Não há nada na lista a ser adicionado.')
+    } else{
+    // item.classList.add('cor')
     lista.appendChild(item)
     document.getElementById('texto-tarefa').value=''
+}
 }
 
 // seleciona elemento da lista ao clicar
     const lista=document.getElementById('lista-tarefas')  
     lista.addEventListener('click',function(event){
-        let selec=document.getElementsByClassName('cor')
         let back=document.getElementsByClassName('selecao')
         console.log(back)
         if(back.length==0){
@@ -23,3 +26,14 @@ function adicionaTarefa(){
         }
     })
 
+// risca o elemento ao ser clicado duas vezes
+lista.addEventListener('dblclick',function(event){
+    let risco=document.getElementsByClassName('completed')
+    console.log(risco)
+    console.log(event.target.className)
+    if(event.target.classList.contains('completed')===true){
+        event.target.classList.remove('completed')
+    } else{
+        event.target.classList.add('completed')
+    }
+})
