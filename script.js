de0740d7d330721e64cbd9d4033c9dc7fd474cd0
Tomas Breuckmann <tomas.yoga@yahoo.com>
2021-12-14 21:58:47 -0300

    fim do dia

--- script.js
+++ script.js
@@ -4,15 +4,21 @@ function adicionaTarefa(){
     let lista=document.getElementById('lista-tarefas')
     let item=document.createElement('li')
     item.innerText=valor;
-    item.style.backgroundColor='none'
+    item.classList.add('cor')
     lista.appendChild(item)
     document.getElementById('texto-tarefa').value=''
 }
 
 function pintaFundo(event){
-    let lista=document.getElementById('lista-tarefas')
+    let lista=document.getElementById('lista-tarefas')  
     lista.addEventListener('click',function(event){
+        let selec=document.getElementsByClassName('cor')
+        console.log(selec)
+        for(let i=0;i<selec.length;i+=1){
+            selec[i].style.backgroundColor='none'
+        }
         event.target.style.backgroundColor='rgb(128, 128, 128)'
     })
 }
 pintaFundo()
+
